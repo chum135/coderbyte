0d6d120cd4a9aac9672c64f1821019f44ebdd6db
chum135 <chum135@naver.com>
2015-07-29 16:01:42 +0900

    Create corderbyte_07.rb

---
+++ corderbyte_07.rb
@@ -0,0 +1,17 @@
+def CheckNums(num1,num2)
+
+  if num1>num2
+    return "false"
+    elsif num1<num2
+    return "true"
+  else
+    return "-1"
+  end
+  
+  
+  
+  
+  # code goes here
+  return num1 
+         
+end
