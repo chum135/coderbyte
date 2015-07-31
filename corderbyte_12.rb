c8eb05afcfcfdcee3eae73f01a77252a3b2bf2db
chum135 <chum135@naver.com>
2015-07-31 14:23:33 +0900

    Create corderbyte_12.rb

---
+++ corderbyte_12.rb
@@ -0,0 +1,12 @@
+def VowelCount(str)
+
+  x_count = str.downcase.count("aeiou")
+  
+  
+  return x_count
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+VowelCount(STDIN.gets)           
